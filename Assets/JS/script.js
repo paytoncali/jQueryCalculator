358861f996924510835147f2d3cd769c3c316bfc
riraq <riraq@mail.usf.edu>
2021-01-20 21:11:40 -0600

    added extra variables; started eventListener for inserting clicked buttons into results area

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,18 +11,23 @@ var addition = document.getElementById("button-plus");
 // var openParen
 // var closeParen
 var button = $("button");
+var firstNumber
+var operator
+var secondNumber
+var result
 // add eventlistener to buttons -> click,function(event)
 button.on("click", function(){
         var elements = this.value;
         console.log("this.value:", elements)
 });
-// >>>> var el = event.target
 //use textContent 
 
 
 // create logic that represents button function: - + * /
 // display each button that is clicked
+
 // ensure no html is overwritten 
-// display items that makeup the mathmatical formula desired ie. "9*9+6" 
+// display items that makeup the mathmatical formula desired ie. "9*9+6"
+$() 
 // display results of math formula 
 // clear results and reset calculator 
